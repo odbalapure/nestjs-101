a3b1674ad040da1c1cd695daaa74cb305970450a
Om Balapure <odbalapure@gmail.com>
2024-06-15 11:37:09 +0530

    Setup JWT with passportjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     AuthModule,
     UserModule,
     BookmarkModule,
-    PrismaModule,
     PrismaModule
   ],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { AuthDto } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) { }
 
     async signup(dto: AuthDto) {
         try {
@@ -23,7 +25,7 @@ export class AuthService {
                 }
             });
 
-            return user;
+            return this.signToken(user.id, user.email);
         } catch (err) {
             if (err instanceof PrismaClientKnownRequestError) {
                 if (err.code === 'P2002') {
@@ -54,6 +56,23 @@ export class AuthService {
 
         delete user.hash;
 
-        return user;
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        };
+
+        const secret = this.config.get('JWT_SECRET');
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret
+        });
+
+        return {
+            access_token: token
+        };
     }
 }
